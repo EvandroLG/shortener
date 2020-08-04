2299e94e326ebe1c4d330238f369d1602db6667e
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2020-08-04 22:26:51 +0200

    create middleware error handle

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import helmet from 'helmet';
@@ -12,6 +12,19 @@ app.use(morgan('tiny'));
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+
+app.use(
+  (
+    error: Error & { status: number; message: string },
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    res.status(error.status ?? 500).json({
+      message: error.message,
+    });
+  }
+);
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
